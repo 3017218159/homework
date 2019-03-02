f1169d2df3ed5a9ddcac736919543945f0799f69
lichen <1010043642@qq.com>
2019-03-02 20:25:16 +0800

    交作业

---
+++ Hello/HelloWorld.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Hello
+{
+    class HelloWorld
+    {
+        public void SayHello1()
+        {
+            Console.WriteLine("Hello World! in a instantiation method.");
+        }
+
+        public static void SayHello2()
+        {
+            Console.WriteLine("Hello World! in a static method.");
+        }
+    }
+}
---
+++ Hello/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Hello World!");
+            Console.ForegroundColor = ConsoleColor.DarkRed;
+            Console.WriteLine("Hello World!");
+            Console.ResetColor();
+            //Console.WriteLine("{0} is {1}", args[0], args[1]);
+
+            //instantiation method
+            HelloWorld myHelloWorld=new HelloWorld();
+            myHelloWorld.SayHello1();
+
+            //static method
+            HelloWorld.SayHello2();
+        }
+    }
+}
